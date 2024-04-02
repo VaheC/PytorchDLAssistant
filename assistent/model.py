64072aac38b4e11e8e2f9963069e36f79a6dbb30
VaheC <vcharchian@gmail.com>
2024-04-02 21:38:03 +0400

    class initiated

---
+++ assistent/model.py
@@ -0,0 +1,163 @@
+class DLAssistant(object):
+    
+    def __init__(self, model, loss_fn, optimizer):
+
+        self.model = model
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.model = self.model.to(self.device)
+
+        self.loss_fn = loss_fn
+        self.optimizer = optimizer
+
+        self.train_loader = None
+        self.valid_loader = None
+        self.tensorboard_writer = None
+
+        self.train_losses = []
+        self.valid_losses = []
+        self.total_epochs = 0
+
+        self.train_step_fn = self._create_train_step_fn()
+        self.valid_step_fn = self._create_valid_step_fn()
+
+    def to(self, device):
+
+        try:
+            self.model = self.model.to(device)
+            self.device = device
+        except RuntimeError:
+            self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+            self.model = self.model.to(self.device)
+            print(f"{device} device is not accessable!!!\n{self.device} is used!!!")
+
+    def set_loaders(self, train_loader, valid_loader=None):
+        self.train_loader = train_loader
+        self.valid_loader = valid_loader
+
+    def set_tensorboard(self, name, log_dir='summary'):
+        
+        if not os.path.exists(log_dir):
+            os.mkdir(log_dir)
+
+        name_suffix = dt.now().strftime('%Y%m%d_%H%M%S')
+        full_name = f"{log_dir}/{name}_{name_suffix}"
+        self.tensorboard_writer = SummaryWriter(full_name)
+
+    def _create_train_step_fn(self):
+
+        def get_train_loss(X, y):
+            self.model.train()
+            y_hat = self.model(X)
+            loss = self.loss_fn(y_hat, y)
+            loss.backward()
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+            return loss.item()
+        
+        return get_train_loss
+    
+    def _create_valid_step_fn(self):
+        
+        def get_valid_loss(X, y):
+            self.model.eval()
+            y_hat = self.model(X)
+            loss = self.loss_fn(y_hat, y)
+            return loss.item()
+        
+        return get_valid_loss
+    
+    def _get_mini_batch_loss(self, validation=False):
+
+        if validation:
+            data_loader = self.valid_loader
+            step_fn = self.valid_step_fn
+        else:
+            data_loader = self.train_loader
+            step_fn = self.train_step_fn
+
+        if data_loader is None:
+            return None
+
+        mini_batch_losses = []
+
+        for X_batch, y_batch in data_loader:
+            X_batch = X_batch.to(self.device)
+            y_batch = y_batch.to(self.device)
+            mini_batch_loss = step_fn(X_batch, y_batch)
+            mini_batch_losses.append(mini_batch_loss)
+
+        return np.mean(mini_batch_losses)
+    
+    def set_seed(self, seed=42):
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+        torch.manual_seed(seed)
+        np.random.seed(seed)
+    
+    def train(self, n_epochs, seed=42):
+
+        self.total_epochs += 1
+
+        self.set_seed(seed)
+
+        for epoch in tqdm(range(n_epochs)):
+
+            train_loss = self._get_mini_batch_loss()
+            self.train_losses.append(train_loss)
+
+            with torch.no_grad():
+                valid_loss = self._get_mini_batch_loss(validation=True)
+                self.valid_losses.append(valid_loss)
+
+            if self.tensorboard_writer is not None:
+                scalar_dict = {'train': train_loss}
+                if self.valid_loader is not None:
+                    scalar_dict.update({'validation': valid_loss})
+                self.tensorboard_writer.add_scalars(
+                    main_tag = 'losses',
+                    tag_scalar_dict = scalar_dict,
+                    global_step = epoch
+                )
+
+        if self.tensorboard_writer is not None:
+            self.tensorboard_writer.flush()
+
+    def show_losses(self):
+        fig = plt.figure(figsize=(10, 4))
+        plt.plot(self.train_losses, label='train', c='blue')
+        if self.valid_loader is not None:
+            plt.plot(self.valid_losses, label='valid', c='orange')
+        plt.yscale('log')
+        plt.xlabel('epoch')
+        plt.ylabel('Loss')
+        plt.legend()
+        plt.show()
+        return fig
+    
+    def save_results(self, filepath):
+        checkpoint_dict = {
+            'model_state_dict': self.model.state_dict(),
+            'optimizer_state_dict': self.optimizer.state_dict(),
+            'total_epochs': self.total_epochs,
+            'train_losses': self.train_losses,
+            'valid_losses': self.valid_losses
+        }
+        torch.save(checkpoint_dict, filepath)
+
+    def load_states(self, filepath):
+        checkpoint_dict = torch.load(filepath)
+        self.model.load_state_dict(checkpoint_dict['model_state_dict'])
+        self.optimizer.load_state_dict(checkpoint_dict['optimizer_state_dict'])
+        self.total_epochs = checkpoint_dict['total_epochs']
+        self.train_losses = checkpoint_dict['train_losses']
+        self.valid_losses = checkpoint_dict['valid_losses']
+
+        self.model.train()
+
+    def predict(self, X):
+        self.model.eval()
+        X_tensor = torch.as_tensor(X).float()
+        X_tensor = X_tensor.to(self.device)
+        pred_y = self.model(X_tensor).detach().cpu().numpy()
+        self.model.train()
+        return pred_y
